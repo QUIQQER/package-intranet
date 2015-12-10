daff1ed4d574d63964e903fc2e61476623c13927
Henning Leutz <leutz@pcsg.de>
2015-12-10 08:48:58 +0100

    code style

--- bin/page/Load.js
+++ bin/page/Load.js
@@ -1,27 +1,23 @@
-
 /**
  * Intranet Loading - page onload event
  *
  * @module package/quiqqer/intranet/bin/page/Load
  * @author www.pcsg.de (Henning Leutz)
  */
-
-define(function()
-{
+define(function () {
     "use strict";
 
-    if ( typeof QUIQQER_LOGIN_FAILED === 'undefined' ) {
+    if (typeof QUIQQER_LOGIN_FAILED === 'undefined') {
         return;
     }
 
-    if ( !QUIQQER_LOGIN_FAILED ) {
+    if (!QUIQQER_LOGIN_FAILED) {
         return;
     }
 
-    require(['qui/QUI'], function(QUI)
-    {
-        QUI.getMessageHandler(function(MH) {
-            MH.addError( QUIQQER_LOGIN_FAILED );
+    require(['qui/QUI'], function (QUI) {
+        QUI.getMessageHandler(function (MH) {
+            MH.addError(QUIQQER_LOGIN_FAILED);
         });
     });
 
